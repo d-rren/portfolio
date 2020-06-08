eae1e0c92aee03b1fc7a73c8762b578cce3f7563
Daniel Lachica <daniel.lachica82@gmail.com>
2020-06-09 00:11:51 +0800

    Add dropdown to navbar

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -2,7 +2,7 @@ var btn = document.getElementById('navbarTogglerBtn');
 var icon = btn.getElementsByClassName('material-icons')[0];
 
 btn.onclick = function() {
-	console.log('clicked');
+	// console.log('clicked');
 	if (icon.textContent == "expand_more") {
 		icon.textContent = "expand_less";
 	}
